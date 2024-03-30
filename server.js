74b4a8fcc19345ced417fa86047225ab178169ab
linikers <linikers@hotmail.com>
2024-03-30 15:10:16 -0300

    add express e comunicacao ok

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
-const express = require("assert");
+const express = require("express");
 const fs = require("fs");
 const app = express();
 
-app.get("ler-arquivo", (req, res) => {
+let listaDados = [];
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("server Online");
+});
+
+app.get("/ler-arquivo", (req, res) => {
   fs.readFile("arquivo.txt", "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return res.staus(500).send("Erro ao ler arquivo");
     }
     res.send(data);
+  });
+});
+
+app.post("/salvar-dados", (req, res) => {
+  const novosDados = req.body;
+
+  listaDados.push(novosDados);
+
+  fs.writeFile("arquivo.txt", JSON.stringify(listaDados), (err) => {
+    if (err) {
+      console.error(err);
+      return res.staus(500).send("Erro ao salvar");
+    }
+    res.send("Dados enviados");
   });
 });
 
